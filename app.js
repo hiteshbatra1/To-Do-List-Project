c98d54487b9693022e26d7ba9f50672c01a4ada8
hitesh batra <hiteshbatra967@gmail.com>
2024-07-24 14:05:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,126 @@
+const inputBox = document.getElementById("inputBox");
+const addBtn = document.getElementById("addBtn");
+const todoList = document.getElementById("todoList");
+
+let editTodo = null;
+
+const addTodo = (e) => {
+  const inputText = inputBox.value.trim();
+  if (inputText.length <= 0) {
+    alert("You Must Write Something In Your To Do...");
+    return false;
+  }
+
+  if (addBtn.value === "Edit") {
+    editLocalTodo(editTodo.target.previousElementSibling.innerHTML);
+    editTodo.target.previousElementSibling.innerHTML = inputText;
+    addBtn.value = "Add";
+    inputBox.value = "";
+  } else {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.innerHTML = inputText;
+    li.appendChild(p);
+
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    editBtn.classList.add("btn", "editBtn");
+    li.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Remove";
+    deleteBtn.classList.add("btn", "deleteBtn");
+    li.appendChild(deleteBtn);
+
+    todoList.appendChild(li);
+    inputBox.value = "";
+
+    saveLocalTodo(inputText);
+  }
+};
+
+const updateTodo = (e) => {
+  // console.log(e.target.innerHTML)
+  if (e.target.innerHTML === "Remove") {
+    // console.log(e.target.parentElement);
+    todoList.removeChild(e.target.parentElement);
+    deleteLocalTodo(e.target.parentElement);
+  }
+
+  if (e.target.innerHTML === "Edit") {
+    // console.log(e.target.previousElementSibling.innerHTML);
+    inputBox.value = e.target.previousElementSibling.innerHTML;
+    inputBox.focus();
+    addBtn.value = "Edit";
+    editTodo = e;
+  }
+};
+
+const saveLocalTodo = (todo) => {
+  let todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  //console.log(todos)
+};
+
+const getLocalTodos = () => {
+  let todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+    todos.forEach((todo) => {
+      const li = document.createElement("li");
+      const p = document.createElement("p");
+      p.innerHTML = todo;
+      li.appendChild(p);
+
+      const editBtn = document.createElement("button");
+      editBtn.innerText = "Edit";
+      editBtn.classList.add("btn", "editBtn");
+      li.appendChild(editBtn);
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerText = "Remove";
+      deleteBtn.classList.add("btn", "deleteBtn");
+      li.appendChild(deleteBtn);
+
+      todoList.appendChild(li);
+    });
+  }
+};
+
+const deleteLocalTodo = (todo) => {
+  let todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  let todoText = todo.children[0].innerHTML;
+  let todoIndex = todos.indexOf(todoText);
+  todos.splice(todoIndex, 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const editLocalTodo = (todo) => {
+  let todos = JSON.parse(localStorage.getItem("todos"));
+  let todoIndex = todos.indexOf(todo);
+  todos[todoIndex] = inputBox.value;
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+addBtn.addEventListener("click", addTodo);
+todoList.addEventListener("click", updateTodo);
